c32f3dceec3d7ddb7736f5120be36a2264d85e05
Brad.Chaiken <brad.chaiken@DET-BCHAIKEN-MAC.local>
2017-11-15 12:08:44 -0500

    Initial storyboard layout, form fields and state picker with logic

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,11 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/*
+    UIPickerViewDataSource and UIPickerViewDelegate are "Protocols".
+ 
+ */
 
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = [
+        "Alaska",
+        "Alabama",
+        "Arkansas",
+        "American Samoa",
+        "Arizona",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "District of Columbia",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Guam",
+        "Hawaii",
+        "Iowa",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Massachusetts",
+        "Maryland",
+        "Maine",
+        "Michigan",
+        "Minnesota",
+        "Missouri",
+        "Mississippi",
+        "Montana",
+        "North Carolina",
+        "North Dakota",
+        "Nebraska",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "Nevada",
+        "New York",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Puerto Rico",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Virginia",
+        "Virgin Islands",
+        "Vermont",
+        "Washington",
+        "Wisconsin",
+        "West Virginia",
+        "Wyoming"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +87,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // When the statePickerBtn is pressed, un hide the statePicker.
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    // We only have one component. Return 1.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Number of rows in picker. Dynamically set by number of items in 'states' array.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    // Text display for rows in picker. Dynamically set by state name.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    // Set statePickerBtn text to state selected from picker.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        
+        // Re hide statePicker.
+        statePicker.isHidden = true
+    }
 
 }
 
